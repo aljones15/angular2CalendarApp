1769da20d2a3475e9e6bef6f2f5b3ef4ed2c95aa
liminal18 <aljones15@gmail.com>
2016-11-29 20:37:40 +0700

    day model is in

---
+++ src/app/models/day.ts
@@ -0,0 +1,9 @@
+export class Day {
+  public type: string;
+  public day: number;
+  public price: number;
+  public selected: boolean;
+  constructor(){
+    this.selected = false;
+  }
+}
--- src/app/services/calendar/calendar.service.ts
+++ src/app/services/calendar/calendar.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Day } from '../../models/day';
 
 @Injectable()
 export class CalendarService {
+  year: number;
+  month: number;
+  daysInMonth: number;
+  day: number;
+  days: Day[];
+  constructor() {
 
-  constructor() { }
+  }
 
 }
