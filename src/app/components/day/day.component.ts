c279ae5684fb383e4dd2e24e78fb998e17f78694
Andrew Jones <Andrew Jones>
2016-12-01 17:00:03 +0700

    sorry just added a link to another tutorial

--- src/app/components/day/day.component.ts
+++ src/app/components/day/day.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CalendarService } from '../../services/calendar/calendar.service';
 import { Day } from '../../models/day';
 // http://stackoverflow.com/questions/37971019/angular-2-detect-scroll-event-from-inner-div
+// https://toddmotto.com/component-events-event-emitter-output-angular-2
 @Component({
   selector: 'app-day',
   templateUrl: './day.component.html',
