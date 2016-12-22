c9b922d0eeddc2106aa67cf3edcebd0602ce5116
liminal18 <aljones15@gmail.com>
2016-12-22 09:09:12 +0700

    small mistake in AppKernel

--- rest_api/app/AppKernel.php
+++ rest_api/app/AppKernel.php
@@ -16,8 +16,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
-            new MonthBundle\MonthBundle(),
-        ]
+            new MonthBundle\MonthBundle()];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
